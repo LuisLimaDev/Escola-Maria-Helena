e14d6a40a5d63918393d571cf6d0d8191320ba0b
LuisLimaDev <56639074+LuisLimaDev@users.noreply.github.com>
2020-02-21 17:00:59 -0300

    Create avisos.js

---
+++ avisos.js
@@ -0,0 +1,15 @@
+// avisos.js
+
+avisos = [
+		fev18 = [
+			"18/02/2020",
+			"COMUNICADO",
+			"A Direção da EE Prof.ª Maria Helena Antônio Cardoso (antigo EE Núcleo Habitacional Vida Nova) vem através deste, comunicar que hoje (18/02/2020), por motivo de falta de água, os alunos foram dispensados às 15:30. <br><span class='assinatura'>Atenciosamente, À Gestão</span>"
+		],
+		fev20 = [
+			"20/02/2020",
+			"COMUNICADO",
+			"A direçãoda E. E. Profª Maria Helena Antônio Cardoso (antigo E. E. Nucleo Habitacional Vida Nova), vem atravéz deste, comunicar que não haverá aula nos seguintes dias:<li>21/02/2002 (6ª feira) - Reunião Pedagógica para a elaboração </li><li>24/02/2020 (2ª feira) - Ponto Facultativo -- Carnaval</li><li>25/02/2020 (3ª feira) - Ponto Facultativo -- Carnaval</li><li>26/02/2020 - (4ª feira) Reunião Pedagógica -- Replanejamento</li> <span class='assinatura'>Atenciosamente, À Gestão</span>"
+		]
+	
+]
